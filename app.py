446088fadbcd3fe679c12ca08ac888c420818627
emmagodson <emmargodson@gmail.com>
2025-05-16 10:27:53 +0000

    Updating app syntax

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     hostname = getenv('HOSTNAME')
-    name = getenv('YOUR_NAME')
+    name = getenv("YOUR_NAME")
     if name is None:
       name = "friend"
     return f"<h1>Hello {name}.</h1>\n\n<h2>I'm currently running in {hostname}.</h2>\n"
